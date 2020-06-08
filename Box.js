978cdb618d46cdb95cd58117cc094e01eeffd798
BhavyaJandhyala <64642658+BhavyaJandhyala@users.noreply.github.com>
2020-06-08 13:42:14 +1000

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,15 @@
+class side{
+    constructor(x,y,width,height){
+    
+    this.body = Bodies.rectangle(x,y,width,height);
+    this.width = width;
+    this.height = height;
+    World.add(world,this.body);
+
+}
+display(){
+    var pos = this.body.position;
+    fill(255);
+    rect(pos.x, pos.y, this.width, this.height);
+}
+}
